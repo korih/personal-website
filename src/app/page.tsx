38cccd212f5a4cef10419a174fb1f58c739cf6be
korih <kori@korih.com>
2025-08-07 14:15:54 -0600

    change url of blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,12 +50,16 @@ export default function Home() {
                     </Link>
                   </div>
                   <div className="flex items-center justify-center mb-4 sm:mb-6">
-                    <Link href="/blogs">
+                    <a
+                      href="https://blog.korih.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <div className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500 font-bold text-xl relative overflow-hidden transition-transform duration-300 group-hover:scale-105">
                         Blogs
                         <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-gradient-to-r from-cyan-400 to-blue-500 transition-all duration-300 group-hover:w-full"></span>
                       </div>
-                    </Link>
+                    </a>
                   </div>
                 </div>
               </CardContent>
