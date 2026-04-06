fe6d170a17030e880071c83f6c0e2f6a3e15ce26
korih <mail@korih.com>
2026-04-05 22:29:41 -0700

    fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./tests/setup.ts"],
     globals: true,
+    typecheck: {
+      tsconfig: "./tsconfig.test.json",
+    },
   },
   resolve: {
     alias: {
