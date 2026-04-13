d8ad2c23ebed63f47efbd38140720284efccf0b7
korih <mail@korih.com>
2026-04-12 17:10:30 -0700

    updated wrangler to allow images

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "*.r2.dev",
       },
+      {
+        protocol: "https",
+        hostname: "media.korih.com",
+      },
     ],
   },
 };
